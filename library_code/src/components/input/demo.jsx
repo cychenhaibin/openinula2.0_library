aa6a9b4b921cdf70aaf3501245e51bda8271015f
fupingyezi <3457432742@qq.com>
2025-07-17 15:09:48 +0800

    fix: 修复非受控input的bug

--- library_code/src/components/input/demo.jsx
+++ library_code/src/components/input/demo.jsx
@@ -1,39 +1,60 @@
 import Input from "./index.jsx";
-let val = '';
-function Demo1() {
-    function handleInput(e) {
-        val = e.target.value;
-      }
 
-    return (
+function Demo1() {
+  let value = "111";
+  let value2 = "222";
+
+  function handleInput(e) {
+    value = e.target.value;
+    console.log("val", value);
+  }
+
+  function handleInput2(e) {
+    value2 = e.target.value;
+    console.log("val2", value2);
+  }
+
+  return (
     <div>
-        <Input />
-        <Input size="large" defaultValue="hello" variant="filled" value={val} onInput={e => val = e.target.value} />
-        <Input size="default" variant="filled" />
-        <Input size="small" variant="filled" />
-        <Input size="large" variant="outlined" />
-        <Input size="default" variant="outlined" />
-        <Input size="small" variant="outlined" />
-        <Input size="large" variant="borderless" />
-        <Input size="default" variant="borderless" />
-        <Input addonBefore="http://" addonAfter=".com" />
-        <div>
+      <Input />
       <Input
-        value={val}
-        onInput={handleInput}
-        showCount
-        maxLength={20}
-        placeholder="请输入内容"
+        size="large"
+        defaultValue="hello"
+        variant="filled"
+        value={value}
+        onInput={(e) => (value = e.target.value)}
       />
-      <Input
-        value={val}
-        onInput={handleInput}
-        placeholder="请输入内容"
-      />
-      {/* <div style={{marginTop: 8, color: '#888'}}>当前内容：{val}</div> */}
+      <Input size="default" variant="filled" />
+      <Input size="small" variant="filled" />
+      <Input size="large" variant="outlined" />
+      <Input size="default" variant="outlined" />
+      <Input size="small" variant="outlined" />
+      <Input size="large" variant="borderless" />
+      <Input size="default" variant="borderless" />
+      <Input addonBefore="http://" addonAfter=".com" />
+      <div>
+        <Input
+          value={value}
+          onInput={handleInput}
+          showCount
+          maxLength={20}
+          placeholder="请输入内容1"
+        />
+        <Input
+          value={value2}
+          onInput={handleInput2}
+          placeholder="请输入内容2"
+        />
+        <Input
+          defaultValue="111"
+          // onInput={handleInput2}
+          showCount
+          placeholder="请输入内容2"
+        />
+        {/* <div style={{marginTop: 8, color: '#888'}}>当前内容：{val}</div> */}
+      </div>
     </div>
-    </div>
-    )
+  );
 }
 
-export default Demo1; 
+export default Demo1;
